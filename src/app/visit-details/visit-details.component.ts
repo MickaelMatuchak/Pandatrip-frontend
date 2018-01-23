1198c896b09bb54cda199c91bc6e21ea0c42d8fb
Benjamin Bermont <benji-du-51@hotmail.fr>
2018-01-23 22:39:50 +0100

    Ajout jQuery et fenêtre modale choisir guide

--- src/app/visit-details/visit-details.component.ts
+++ src/app/visit-details/visit-details.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {VisitDetailsModel} from "./visit-details.model";
 
+declare var $: any;
+declare var jquery:any;
+
 const VISITSDETAILS: VisitDetailsModel =
   { id: 1, name: 'Arc de Triomphe', address: '1 rue mock', city: 'Paris', postalCode: 93000, description: "L’arc de triomphe de l’Étoile souvent appelé simplement l'Arc de Triomphe, dont la construction, décidée par l'empereur Napoléon Ier, débuta en 1806 et s'acheva en 1836 sous Louis-Philippe, est situé à Paris, dans le 8e arrondissement. Il s'élève au centre de la place Charles-de-Gaulle (anciennement place de l’Étoile), dans l'axe et à l’extrémité ouest de l’avenue des Champs-Élysées, à 2,2 kilomètres de la place de la Concorde"};
 
@@ -10,11 +13,30 @@ const VISITSDETAILS: VisitDetailsModel =
   styleUrls: ['./visit-details.component.css', '../../../node_modules/bulma/css/bulma.css', "../../../node_modules/font-awesome/css/font-awesome.css"]
 })
 export class VisitDetailsComponent implements OnInit {
-
   visitDetails = VISITSDETAILS;
-  constructor() {  }
+
+  /* afficher pop-up*/
+  toggleModalClasses(event) {
+    let modalId = event.currentTarget.dataset.modalId;
+    let modal = $(modalId);
+    modal.toggleClass('is-active');
+    $('html').toggleClass('is-clipped');
+  };
+
+
+
+  constructor() {
+  }
 
   ngOnInit() {
+    $('.open-modal').click(this.toggleModalClasses);
+    $('.close-modal').click(this.toggleModalClasses);
+
+    $('tr').on('click', function(){
+      $('tr').removeClass('selected');
+      $(this).addClass('selected');
+    });
   }
+
 
 }
