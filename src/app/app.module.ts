4a2e213486743053c4c8fd25bf0d9a0fce129bff
Benjamin Bermont <benji-du-51@hotmail.fr>
2018-01-24 16:18:45 +0100

    Ajout star-rating-component pour affichage note

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { ReviewsComponent } from './reviews/reviews.component';
 import { ProfilComponent } from './profil/profil.component';
 import { RecapComponent } from './recap/recap.component';
 import { PartnersComponent } from './partners/partners.component';
+import { StarRatingModule } from 'angular-star-rating';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -55,7 +56,7 @@ const routes: Routes = [
     PartnersComponent
   ],
   imports: [
-    BrowserModule, HttpModule, RouterModule.forRoot(routes)
+    BrowserModule, HttpModule, RouterModule.forRoot(routes), StarRatingModule.forRoot()
   ],
   providers: [ AppService ],
   bootstrap: [ AppComponent ]
--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReviewsModel } from "./reviews.model";
+import { OnClickEvent, OnRatingChangeEven, OnHoverRatingChangeEvent } from "./../../../node_modules/angular-star-rating/star-rating-struct";
 
 const REVIEWS: ReviewsModel[] = [
   { id: 1, author: 'Jean', avatar:'boy.png', note: 5, title: "Vraiment pas mal...", text: "A la télé, on se rend pas compte à quel point le monument est imposant. Dire que l'arc de triomphe se tient là, débout, depuis des siècles et qu'il n'a presque pas pris une ride donne vraiment le vertige. A voir absolument au moins une fois dans sa vie !", date: '20/01/2018'},
@@ -9,11 +10,30 @@ const REVIEWS: ReviewsModel[] = [
 @Component({
   selector: 'reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.css', '../../../node_modules/bulma/css/bulma.css']
+  styleUrls: ['./reviews.component.css', '../../../node_modules/bulma/css/bulma.css', './star-rating.scss']
 })
 export class ReviewsComponent implements OnInit {
 
   reviews = REVIEWS;
+
+  onClickResult:OnClickEvent;
+  onHoverRatingChangeResult:OnHoverRatingChangeEvent;
+  onRatingChangeResult:OnRatingChangeEven;
+
+  onClick = ($event:OnClickEvent) => {
+    console.log('onClick $event: ', $event);
+    this.onClickResult = $event;
+  };
+
+  onRatingChange = ($event:OnRatingChangeEven) => {
+    console.log('onRatingUpdated $event: ', $event);
+    this.onRatingChangeResult = $event;
+  };
+
+  onHoverRatingChange = ($event:OnHoverRatingChangeEvent) => {
+    console.log('onHoverRatingChange $event: ', $event);
+    this.onHoverRatingChangeResult = $event;
+  };
 
   constructor() { }
 
