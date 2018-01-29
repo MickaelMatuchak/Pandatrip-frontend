800fda0a0e94479997962d7700d17572dc6f933c
Benjamin Bermont <benji-du-51@hotmail.fr>
2018-01-29 15:51:54 +0100

    Surcharge css carousel pour centrer puces

--- src/app/visit-details/visit-details.component.ts
+++ src/app/visit-details/visit-details.component.ts
@@ -82,6 +82,7 @@ export class VisitDetailsComponent implements OnInit {
     $('tr').on('click', function () {
       $('tr').removeClass('selected');
       $(this).addClass('selected');
+
     });
   }
 
