0f9077d07695742156d79ca6e1ee80b3b172d2f3
Benjamin Bermont <benji-du-51@hotmail.fr>
2018-01-30 16:52:02 +0100

    Ajout dynamique d'une visite au panier

--- src/app/visit-details/visit-details.component.ts
+++ src/app/visit-details/visit-details.component.ts
@@ -7,6 +7,7 @@ import {ImageModel} from '../image/image.model';
 import {ReviewsModel} from '../reviews/reviews.model';
 
 import {ICarouselConfig, AnimationConfig} from 'angular4-carousel';
+import {ItemVisitModel} from "./item-visit.model";
 
 declare var $: any;
 declare var jquery: any;
@@ -24,11 +25,10 @@ export class VisitDetailsComponent implements OnInit {
   public imageSources: string[] = [];
   private sub: any;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private visitService: VisitService
-  ) {}
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private visitService: VisitService) {
+  }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
@@ -84,6 +84,9 @@ export class VisitDetailsComponent implements OnInit {
       $(this).addClass('selected');
 
     });
+
+    this.afficherPanier();
+
   }
 
   ngOnDestroy() {
@@ -106,5 +109,114 @@ export class VisitDetailsComponent implements OnInit {
     let modal = $(modalId);
     modal.toggleClass('is-active');
     $('html').toggleClass('is-clipped');
-  };
+  }
+
+  stockerEnSession(visit: ItemVisitModel) {
+    let retrievedObject = null;
+
+    if (localStorage.getItem('visits') != null) {
+      retrievedObject = JSON.parse(localStorage.getItem('visits'));
+    }
+
+    // Sauvegarder les informations dans l'espace local courant
+
+    if (retrievedObject != null) {
+      let isExist = false;
+
+      retrievedObject.forEach(function (element) {
+        if (element.name == visit.name) {
+          isExist = true;
+        }
+      });
+
+      if (isExist == false) {
+        retrievedObject.push(visit);
+      }
+    } else {
+      retrievedObject = [visit];
+    }
+
+    localStorage.setItem("visits", JSON.stringify(retrievedObject));
+  }
+
+  ajouterPanier() {
+    let visit = new ItemVisitModel(this.visitSelected["name"], null);
+
+    this.stockerEnSession(visit);
+
+    this.afficherPanier();
+  }
+
+  ajouterPanierGuide() {
+    let trSelected = document.getElementsByClassName("selected")[0];
+    let guideBalise = trSelected.getElementsByClassName("guide")[0];
+
+    let guide = null;
+
+    if(guideBalise != undefined) {
+      guide = guideBalise.textContent;
+    }
+
+    let visit = new ItemVisitModel(this.visitSelected["name"], guide);
+
+    this.stockerEnSession(visit);
+
+    this.afficherPanier();
+  }
+
+  afficherPanier() {
+
+    $(".panier_item").remove();
+
+    let retrievedObject = null;
+
+    if (localStorage.getItem('visits') != undefined) {
+      retrievedObject = JSON.parse(localStorage.getItem('visits'));
+
+      retrievedObject.forEach(function (element) {
+        let div = document.createElement("div");
+        div.className = "panier_item";
+
+        let span = document.createElement("span");
+        span.className = "panier_supprimer";
+
+        let i = document.createElement("i");
+        i.className = "fa fa-window-close";
+        i.setAttribute("_ngcontent-c2", "")
+
+        span.appendChild(i);
+
+        let h4 = document.createElement("h4");
+        h4.setAttribute("_ngcontent-c2", "");
+        let titre = document.createTextNode(element.name);
+        h4.appendChild(titre);
+
+        let p = document.createElement("p");
+        p.setAttribute("_ngcontent-c2", "");
+        p.className = "content";
+
+        let contenu;
+
+        if(element.guide != null){
+          console.log(element.guide);
+          contenu = document.createTextNode(element.guide);
+        } else {
+          contenu = document.createTextNode("Visite non guidée (Envie d'un guide ?)");
+        }
+
+        p.appendChild(contenu);
+
+        div.appendChild(span);
+        div.appendChild(h4);
+        div.appendChild(p)
+
+        let hr = document.createElement("hr");
+        hr.setAttribute("_ngcontent-c2", "");
+
+        let panier = document.getElementById("mon_panier");
+        panier.appendChild(div);
+        div.appendChild(hr);
+      });
+    }
+  }
 }
